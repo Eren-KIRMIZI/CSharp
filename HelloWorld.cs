90098f8100e9675ec1338e59854cbd691cebb5b0
Eren KIRMIZI <erencoding94@gmail.com>
2025-11-28 18:46:51 +0300

    Create HelloWorld.cs

---
+++ HelloWorld.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp
+{
+    internal class HelloWorld
+    {
+        static void Main(String[] Args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
