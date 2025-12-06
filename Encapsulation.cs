64fa95c7d042c4f68c9ddd468b3f114f92d3eed5
Eren KIRMIZI <erencoding94@gmail.com>
2025-12-06 17:02:21 +0300

    Create Encapsulation.cs

---
+++ Encapsulation.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp
+{
+    class Person
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person myObj = new Person();
+            myObj.Name = "Liam";
+            Console.WriteLine(myObj.Name);
+        }
+    }
+}
